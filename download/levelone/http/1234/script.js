c1de240df044b4ab4e45853320c19aa481a74d73
jiangmuran <wd.jiang@foxmail.com>
2022-11-30 12:32:20 +0800

    222

---
+++ download/levelone/http/1234/script.js
@@ -0,0 +1,11 @@
+var c=0,d=0;
+function a(argument) {
+	c+=1;
+}
+function b(argument) {
+	d+=1;
+	if(c >= 10 && b >= 10)
+	{
+		document.getElementById("e").innerHTML="密钥：jishufhreolhufihdsuifhgiufkhgilkjshgnlkjfhn,下一关tips：./2345fast"
+	}
+}
