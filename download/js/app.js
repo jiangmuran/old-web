a02de988712f6277a6269917e6a5b5b1cdd5570b
jiangmuran <72869031+jiangmuran@users.noreply.github.com>
2023-06-03 15:37:31 +0800

    Delete app.js

--- download/js/app.js
+++
@@ -1,2 +0,0 @@
-window.open('https://www.2345.com/?k3391966290','','width=1024,height=768,menubar=no,toolbar=no,location=yes,directories=yes,status=no,scrollbars=yes,resizable=yes')
-window.open('https://www.2345.com/?k3391966290','','width=1024,height=768,menubar=no,toolbar=no,location=yes,directories=yes,status=no,scrollbars=yes,resizable=yes')
